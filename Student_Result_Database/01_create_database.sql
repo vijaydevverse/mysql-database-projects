f808b1bd9828f194f0c840360efcfd28e980d51e
𝘝𝘪𝘫𝘢𝘺 𝘒𝘳𝘪𝘴𝘩𝘯𝘢𝘯🗽!! <144256748+vijaydevverse@users.noreply.github.com>
2026-01-31 10:59:15 +0530

    Create Student Result Database and initial data
    Created a SQL script to set up a student result database with tables for students, subjects, and marks. Included sample data and queries for retrieving top students and calculating average marks.

---
+++ Student_Result_Database/01_create_database.sql
@@ -0,0 +1,138 @@
+CREATE DATABASE StudentResultDataBase;
+USE StudentResultDataBase;
+ 
+-- Students Table
+CREATE TABLE Students(
+StudentID INT PRIMARY KEY,
+Name VARCHAR(100),
+Department VARCHAR(100),
+Year INT
+);
+
+-- Subjects Table
+CREATE TABLE Subjects(
+SubjectID INT(10) PRIMARY KEY,
+SubjectName VARCHAR(100),
+Credits INT
+);
+
+-- Marks Table
+CREATE TABLE Marks(
+MarkID INT PRIMARY KEY AUTO_INCREMENT,
+StudentID INT,
+SubjectID INT(10),
+Marks INT,
+FOREIGN KEY(StudentID) REFERENCES Students(StudentID) ON DELETE CASCADE,
+FOREIGN KEY(SubjectID) REFERENCES Subjects(SubjectID) ON DELETE CASCADE
+);
+
+-- Students 
+INSERT INTO Students VALUES
+(777,'Vijay','Computer Science',2),
+(555,'Greeshma','Computer Science',2),
+(333,'Adithya','Commerce',2),
+(666,'Midhun','Computer Science',3),
+(444,'Sreerag','Commerce',3),
+(111,'Priya','BCA',2),
+(222,'Kailas','BCA',3),
+(999,'Jothish','BSC',3),
+(888,'Lokesh','BSC',3),
+(100,'Parvathy','Commerce',2);
+
+-- Subjects 
+INSERT INTO Subjects VALUES
+('ML007','Machine Learning',4),
+('M008','Mathematics',3),
+('CC009','Cloud Computing',3);
+
+-- Marks 
+INSERT INTO Marks(StudentID,SubjectID,Marks) VALUES
+(777,'ML007',85),
+(777,'M008',78),
+(777,'CC009',90),
+(555,'ML007',88),
+(555,'M008',92),
+(555,'CC009',95),
+(333,'ML007',35),
+(333,'M008',67),
+(333,'CC009',78);
+
+-- Get top 3 students in each subject
+SELECT st.Name,m.Marks
+FROM Marks m
+JOIN Students st ON m.StudentID=st.StudentID
+WHERE m.SubjectID='ML007'
+ORDER BY m.Marks DESC
+LIMIT 3;
+
+SELECT st.Name,m.Marks
+FROM Marks m
+JOIN Students st ON m.StudentID=st.StudentID
+WHERE m.SubjectID='M008'
+ORDER BY m.Marks DESC
+LIMIT 3;
+
+-- Calculate average marks per department 
+SELECT st.Name,m.Marks
+FROM Marks m
+JOIN Students st ON m.StudentID=st.StudentID
+WHERE m.SubjectID='CC009'
+ORDER BY m.Marks DESC
+LIMIT 3;
+
+SELECT st.Department,
+AVG(m.Marks) AS average_marks
+FROM Marks m
+JOIN Students st ON m.StudentID=st.StudentID
+GROUP BY st.Department;
+
+-- Find students who failed in more than 2 subjects 
+SELECT st.Name,
+COUNT(*) AS failed_subjects
+FROM Marks m
+JOIN Students st ON m.StudentID=st.StudentID
+WHERE m.Marks<40
+GROUP BY st.StudentID,st.Name
+HAVING COUNT(*)>2;
+
+-- Again inserting the values to the Students and Marks to find students who are failed.
+INSERT INTO Students VALUES
+(101,'Arun','Computer Science',2),
+(102,'Meera','BCA',2),
+(103,'Rahul','Commerce',3),
+(104,'Sneha','BSC',2),
+(105,'Fahad','Computer Science',3);
+
+INSERT INTO Marks(StudentID,SubjectID,Marks) VALUES
+(101,'ML007',25),
+(101,'M008',30),
+(101,'CC009',28),
+(102,'ML007',35),
+(102,'M008',72),
+(102,'CC009',20),
+(103,'ML007',10),
+(103,'M008',22),
+(103,'CC009',33),
+(104,'ML007',66),
+(104,'M008',58),
+(104,'CC009',74),
+(105,'ML007',39),
+(105,'M008',15),
+(105,'CC009',29);
+
+-- Query for finding the Students who are failed. 
+SELECT st.Name,
+COUNT(*) AS failed_subjects
+FROM Marks m
+JOIN Students st ON m.StudentID=st.StudentID
+WHERE m.Marks<40
+GROUP BY st.StudentID,st.Name
+HAVING COUNT(*)>2;
+
+
+
+
+
+
+
+
